cb8e017df500f00334cb9d13ea486073018bba02
Niels Koster <n.koster@portavita.eu>
2021-09-23 23:28:09 +0200

    Magic number 17.32050808070670837

--- src/context/State/reducer.js
+++ src/context/State/reducer.js
@@ -1,5 +1,5 @@
 export const initialState = {
-  frequency: 145.3, waveLength: 17.32, velocityFactor: 100
+  frequency: 145.3, waveLength: 17.32050808070670837, velocityFactor: 100
 }
 
 export const StateReducer = (state = initialState, action) => {
