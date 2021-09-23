a7159a061ac298eec016b1d53e1eb74f8071e23c
Niels Koster <n.koster@portavita.eu>
2021-09-23 23:02:28 +0200

    Use Roboto

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import './index.css'
 import 'regenerator-runtime'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 import App from './App'
 
