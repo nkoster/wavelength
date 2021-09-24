ec79e0915419e7082b0e3554b2b8150221d911d0
Niels Koster <n.koster@portavita.eu>
2021-09-24 19:44:00 +0200

    Bug: should be (velocityFactor / 100)

--- src/components/InputFrequency/index.js
+++ src/components/InputFrequency/index.js
@@ -14,11 +14,11 @@ const InputFrequency = _ => {
   }
 
   const calculatedWaveLength = 300000000 /
-    (frequency * 1000000) * (100 / velocityFactor)
+    (frequency * 1000000) * (velocityFactor / 100)
 
   return (
     <div style={InputStyle}>
-      <Card style={CardStyle}>
+      <Card style={CardStyle} elevation={4}>
         <Typography style={TypographyStyle}>
           <i style={{fontSize: '22px', paddingRight: '10px'}}>λ</i>
           {calculatedWaveLength} m
@@ -27,8 +27,8 @@ const InputFrequency = _ => {
           type='number'
           inputProps={{ min: 0 }}
           value={frequency}
-          label={<div style={{marginTop: '-6px'}}>
-            <i style={{fontSize: '22px', paddingRight: '10px'}}>ƒ</i>
+          label={<div style={{marginTop: '-6px', fontSize: '22px', marginRight: '-46px'}}>
+            <i style={{paddingRight: '10px'}}>ƒ</i>
             Frequency in MHz
           </div>}
           variant='outlined'
@@ -47,7 +47,7 @@ const InputStyle = {
 }
 
 const CardStyle = {
-  margin: '10px',
+  margin: '20px',
   padding: '20px'
 }
 
